b50334ebe1358da68d673a196b49ce0c6f59e1d3
nikachu404 <yasinskaa.veronika@gmail.com>
2023-05-28 17:44:38 +0300

    added date input

--- src/components/TrainForm/TrainForm.tsx
+++ src/components/TrainForm/TrainForm.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
+import { getCurrentDate } from '../../helpers';
 import './TrainForm.scss';
 
-export const TrainForm: React.FC<{
+type Props = {
   onSearch: (departure: string, arrival: string) => void;
-}> = ({ onSearch }) => {
+};
+
+export const TrainForm: React.FC<Props> = ({ onSearch }) => {
   const [departure, setDeparture] = useState('');
   const [arrival, setArrival] = useState('');
+  const [date, setDate] = useState('');
 
   const handleDepartureChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setDeparture(e.target.value);
@@ -13,6 +17,11 @@ export const TrainForm: React.FC<{
 
   const handleArrivalChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setArrival(e.target.value);
+  };
+
+  const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDate(e.target.value);
+    console.log(e.target.value);
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -41,6 +50,18 @@ export const TrainForm: React.FC<{
           placeholder="Куди"
           value={arrival}
           onChange={handleArrivalChange}
+          required
+        />
+      </div>
+      <div>
+        <input
+          type="date"
+          className="train-form__input"
+          id="dateInput"
+          placeholder="Дата"
+          value={date}
+          min={getCurrentDate()}
+          onChange={handleDateChange}
           required
         />
       </div>
