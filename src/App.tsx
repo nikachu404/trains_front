d55191f5eb9863192958f3b412781aee620997d3
nikachu404 <yasinskaa.veronika@gmail.com>
2023-05-28 13:35:46 +0300

    added components to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Train } from './types/Train';
+import { TrainForm, TrainList } from './components';
 import './App.scss';
 
 export const App: React.FC = () => {
-  return <div>Hello</div>;
+  const [trains, setTrains] = useState<Train[]>([]);
+
+  const handleSearch = async (departure: string, arrival: string) => {
+    try {
+      const response = await axios.get<Train[]>(
+        `https://trains-node-js.herokuapp.com/trains?departure=${departure}&arrival=${arrival}`
+      );
+      setTrains(response.data);
+    } catch (error) {
+      console.error('Error fetching trains:', error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Train Page</h1>
+      
+      <TrainForm onSearch={handleSearch} />
+      {trains.length > 0 && <TrainList trains={trains} />}
+    </div>
+  );
 };
