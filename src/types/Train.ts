90b56756b8abe1f46233d929e78db62422af85e9
nikachu404 <yasinskaa.veronika@gmail.com>
2023-05-28 13:17:07 +0300

    added types

---
+++ src/types/Train.ts
@@ -0,0 +1,10 @@
+export interface Train {
+  id: number;
+  departure_station: string;
+  arrival_station: string;
+  departure_time: string;
+  arrival_time: string;
+  train_number: string;
+  capacity: number;
+  fare: number;
+}
